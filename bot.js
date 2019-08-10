2532ddf607461013c1496eb11292f9a4c3213f9e
warhammercasey <warhammercasey@gmail.com>
2019-08-09 21:30:27 -0600

    create bot.js

---
+++ bot.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.login(process.env.BOT_TOKEN);
+
+
+client.on('ready', () => {
+    console.log('Ready');
+});
+
+client.on('message', message => {
+
+});
