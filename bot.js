96d3000af01df71ea743d4dfe9b0a4d020e28f19
warhammercasey <warhammercasey@gmail.com>
2019-08-09 21:49:43 -0600

    test

--- bot.js
+++ bot.js
@@ -1,12 +1,29 @@
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
-client.login(process.env.BOT_TOKEN);
+
 
 
 client.on('ready', () => {
-    console.log('Ready');
+
+    console.log('I am ready!');
+
 });
 
-//client.on('message', message => {
 
-//});
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+        message.reply('pong');
+
+    }
+
+});
+
+
+
+// THIS  MUST  BE  THIS  WAY
+
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
